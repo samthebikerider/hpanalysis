c1ba11b7040fb7232672ef7a4cedd9d860b32893
samthebikerider <samthebikerider@gmail.com>
2023-07-26 15:33:05 -0700

    Update raw_data_bind_sites.R

--- raw_data_bind_sites.R
+++ raw_data_bind_sites.R
@@ -15,7 +15,12 @@
 rm(list = ls())
 
 # packages
-source()
+if(Sys.info()[7] == "rose775"){
+  source("/Users/rose775/Library/CloudStorage/OneDrive-PNNL/Desktop/Projects/ccHP/hpanalysis/functions_for_use.R")
+} else{
+  print("Kevin please replace this print with your path to hpanalysis so you can load the functions using source")
+}
+
 library(tidyverse)
 library(readxl)
 library(data.table)
@@ -62,14 +67,23 @@ setwd("~") # reset wd
 setwd(michaels_wd) # set wd to michaels raw data
 
 michaels_files <- list.files()
-# michaels_site_IDs <- unique(substr(michaels_files, 14, 19))
-michaels_site_IDs <- c("8220XE", "8726VB", "9944LD")
+michaels_site_IDs <- unique(substr(michaels_files, 14, 19))
 
 for (i in michaels_site_IDs){
   setwd("~") # reset wd
   setwd(michaels_wd) # set wd to michaels raw data
   df <- list.files(path = michaels_wd, pattern = i) %>%
            map_df(~fread(.)) # read all files for the current site iteration and bind_rows to single df
+  df <- df %>%
+    rowwise() %>% 
+    mutate(Aux_Power = sum(c_across(colnames(df)[colnames(df) %in% c("Aux1_Power", "Aux2_Power", "Aux3_Power", "Aux4_Power")]), na.rm = T)) # sum for aux power
+  to_keep <- c("index", "HP_Power", "Fan_Power", "AHU_Power", "Aux_Power",
+               "OA_TempF", "OA_RH", "SA_temp_blower_cabinet_F", "SA_RH_blower_cabinet",
+               "SA1_TempF", "SA1_RH", "SA2_TempF", "SA2_RH", "SA3_TempF", "SA3_RH",
+               "SA4_TempF", "SA4_RH", "RA_TempF", "RA_RH", "AHU_TempF", "AHU_RH",
+               "Room1_TempF", "Room1_RH","Room2_TempF", "Room2_RH", "Room3_TempF",
+               "Room3_RH", "Room4_TempF", "Room4_RH", "RV_Volts") # list of cols used in agg
+  df <- subset(df, select = names(df) %in% to_keep) # keep only cols  used in agg
   
   setwd("~") # reset wd
   setwd(sites_wd) # set wd to sites folder for saving single site file
