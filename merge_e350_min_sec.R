8e77bb77869fbade7b95f3e8170118cefe03292d
samthebikerider <samthebikerider@gmail.com>
2023-08-08 10:53:50 -0700

    updates to merger e350

--- merge_e350_min_sec.R
+++ merge_e350_min_sec.R
@@ -42,19 +42,20 @@ df_sec <- read_csv(paste("site_", i, "_second.csv", sep = ""))
 df_sec$`Timestamp (UTC)` <- parse_date_time(df_sec$`Timestamp (UTC)`, orders = c("ymd_HMS", "mdy_HMS"))
 df_sec$`Timestamp (UTC)` <- as.POSIXct(df_sec$`Timestamp (UTC)`)
 df_sec <- df_sec %>% drop_na(`Timestamp (UTC)`)
-df_sec_full <- fill_missing_timestamps(df_sec, "Timestamp (UTC)", format = "%y-%m-%d %H:%M:%S", interval = "sec")
-
+df_sec_full <- fill_missing_timestamps(df_sec, "Timestamp (UTC)", format = "%Y-%m-%d %H:%M:%S", interval = "sec")
+df_sec_full$timestamp <- as.POSIXct(df_sec_full$timestamp, format = "%Y-%m-%d %H:%M:%S")
 
 setwd(e350_min_sites_wd)
 df_min <- read.csv(paste("site_", i, "_minute.csv", sep = ""))
 df_min$Timestamp..UTC. <- parse_date_time(df_min$Timestamp..UTC., orders = c("ymd_HMS", "mdy_HMS"))
 df_min$Timestamp..UTC. <- as.POSIXct(df_min$Timestamp..UTC.)
 df_min <- df_min %>% drop_na(Timestamp..UTC.)
-df_min_full <- fill_missing_timestamps(df_min, "Timestamp..UTC.", format = "%y-%m-%d %H:%M:%S", interval = "sec")
+df_min_full <- fill_missing_timestamps(df_min, "Timestamp..UTC.", format = "%Y-%m-%d %H:%M:%S", interval = "sec")
+df_min_full$timestamp <- as.POSIXct(df_min_full$timestamp, format = "%Y-%m-%d %H:%M:%S")
 
 df <- left_join(df_sec_full, df_min_full, by = "timestamp")
 
-
+dups <- subset(df, duplicated(df$timestamp))
 
 # for (i in sites){
 #   setwd(e350_sec_sites_wd)
